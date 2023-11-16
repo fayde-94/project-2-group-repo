b136c85956957aa1bbe998e6bdaf10d042147098
Ceejy-S <96978523+Ceejy-S@users.noreply.github.com>
2023-11-16 15:33:42 -0700

    changes made to html and css added lamplight html and css

--- scripts/loadItems.js
+++ scripts/loadItems.js
@@ -1,6 +1,9 @@
 $(function () {
   $("#navBar").load("navBar.html");
 });
+$(function () {
+  $("#LampLight").load("LampLight.html");
+});
 
 $(function () {
   $("#installFancySwitch").load("fancySwitch.html");
