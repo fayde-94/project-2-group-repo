68dd57ff088994bc01d573bab0d2a4d86536ec13
Ceejy-S <96978523+Ceejy-S@users.noreply.github.com>
2023-11-17 10:03:52 -0700

    changes to js and also index.html

--- scripts/LampLight.js
+++ scripts/LampLight.js
@@ -1,104 +1,76 @@
-const delay = (milliseconds) =>
-  new Promise((resolve) => setTimeout(resolve, milliseconds));
-
 const socket = new WebSocket("ws://homeassistant.local:8123/api/websocket");
+var idNumber = 1;
 
 socket.onopen = (event) => {
-  //console.log("WebSocket connection opened:", event);
+  console.log("WebSocket connection opened:", event);
 
   // Authenticate with Home Assistant
   socket.send(
     JSON.stringify({
       type: "auth",
       access_token:
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI1NGI2YTM5ZjhiZDU0ZjIxOGFlYzk5ZWNmYTA1OTU0NiIsImlhdCI6MTY5OTU2NjQzMSwiZXhwIjoyMDE0OTI2NDMxfQ.QbRxVeZxj0GxG-_4DvdoZrXlzfcUWBr-Dx0zr3Pt4GI", // Replace with your access token
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhMmM3MWI4YTdjZjU0NTdkOWVmZmVjMTI3ZTcyNmM0YyIsImlhdCI6MTcwMDI0MDU3NiwiZXhwIjoyMDE1NjAwNTc2fQ.qU9qQdmwScJHjUrVa9eSNRUUvaoMPUFfjzteBh-YYLw"
+  );
+
+  // Subscribe to events (optional)
+  socket.send(
+    JSON.stringify({
+      type: "subscribe_events",
     })
   );
 };
 
-let switchState;
-let manualToggle = false; // Flag to track manual switch toggles
-//process recieved messages
 socket.onmessage = (event) => {
-  try {
-    const receivedData = JSON.parse(event.data);
-    if (receivedData.type === "result" && Array.isArray(receivedData.result)) {
-      const resultArray = receivedData.result;
-      let background = document.getElementById("background");
-      for (let i = 0; i < resultArray.length; i++) {
-        let currentEntry = resultArray[i];
-        if (currentEntry.entity_id === "switch.thing2") {
-          //when we find thing2......
-          switchState = currentEntry.state;
-          const iotThing = document.getElementById("switch");
-          if (switchState === "on") {
-            //if its on, check the toggleswithc, and add "on" to the body
-            iotThing.checked = true;
-            background.classList.remove("on", "off");
-            background.classList.add("on");
-          } else {
-            iotThing.checked = false; //if its off, check the toggle switch, and add "off" to the body
-            background.classList.remove("on", "off");
-            background.classList.add("off");
-          }
-          break;
-        }
-      }
-    } else {
-      console.warn(
-        "Received data does not match the expected format:",
-        receivedData
-      );
-    }
-  } catch (error) {
-    console.error("Error parsing JSON:", error);
-  }
-  manualToggle = false; // make sure we know the button hasnt been pushed in the last wee bit
+  const data = JSON.parse(event.data);
+  console.log("Received message:", data);
+
+  // Process received messages if needed
 };
-//closing the websocket
+
 socket.onclose = (event) => {
   console.log("WebSocket connection closed:", event);
 };
-//sending message to the pi
+
 function sendMessage(message) {
-  //console.log("Sending message:", message);
+  console.log("Sending message:", message);
   socket.send(message);
 }
-// start a counter so theres a different id every time
-let incrimentalId = 1;
 
-//get the current state of the switch
-function getCurrentSwitchState() {
+function turnOnSwitch() {
+  // Example: Send a command to turn on Switch
   const message = JSON.stringify({
-    id: incrimentalId,
-    type: "get_states",
+    id: idNumber,
+    type: "call_service",
+    domain: "switch",
+    service: "turn_on",
+    service_data: {
+      entity_id: "switch.thing2", // Replace with your switch entity ID
+    },
   });
-  incrimentalId++;
+  idNumber++;
   sendMessage(message);
 }
 
-// Call getCurrentSwitchState every 2 seconds, unless theres been a manual switch recently
-setInterval(() => {
-  if (!manualToggle) {
-    getCurrentSwitchState();
-  }
-}, 2000);
-
-// Call getCurrentSwitchState immediately when the page loads
-getCurrentSwitchState();
-
-//tell the switch to toggle
-function toggleSwitch() {
-  manualToggle = true;
+function turnOffSwitch() {
+  // Example: Send a command to turn off Switch
   const message = JSON.stringify({
-    id: incrimentalId,
+    id: idNumber,
     type: "call_service",
     domain: "switch",
-    service: "Toggle",
+    service: "turn_off",
     service_data: {
-      entity_id: "switch.thing2", // Replace with whatever the entity ID is when copying
+      entity_id: "switch.thing2", // Replace with your switch entity ID
     },
   });
+  idNumber++;
   sendMessage(message);
-  incrimentalId++;
+}
+
+function flickSwitch(buttonID) {
+  const button = document.getElementById(buttonID);
+  if (button.checked == true) {
+    turnOnSwitch();
+  } else if (button.checked == false) {
+    turnOffSwitch();
+  }
 }
