8e5559d1dca0d0b608a289fcadffcfae796c2055
HenryYenHa <henryha@live.ca>
2023-12-01 13:29:21 -0700

    Final version for Demo3

--- jsFiles/iotControls.js
+++ jsFiles/iotControls.js
@@ -6,6 +6,8 @@ const accessToken =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhOGNiMTNjZTJiN2Y0ZDFjYjk0MWM1YzFjYTRmN2YyMSIsImlhdCI6MTcwMDI1MDQxNywiZXhwIjoyMDE1NjEwNDE3fQ.xc0OTLmb-UVyHwM-ts1HP36neodPU5t4UzSy0i8OJsQ";
 let idNumber = 1; //Initial ID number
 let interactFlag = true; //Interact Flag to prevent overwrites
+let isFinallyConnected = false; //Prevent requesting of read states before connection established
+let debugFlag = true; //Debug Flag
 
 //Global variables to check the status of devices
 let statDevice = ""; //Global Variable to seek device's information
@@ -24,11 +26,26 @@ const washroomLights = "tempFakeWash"; //Name of incoming new switch light 4; li
 const motionSensor = "binary_sensor.presence_sensor_fp2_1708_presence_sensor_1"; //Name of Motion Sensor
 const luminSensor = "sensor.presence_sensor_fp2_1708_light_sensor_light_level"; //Name of Light sensor
 const weightSensor = "sensor.smart_scale_c1_real_time_weight"; //Name of Weight sensor
-
+const humiditySensor = "sensor.temperature_sensor_humidity_sensor"; //Name of Humidity Sensor
+const temperatureSensor = "sensor.temperature_sensor"; //Name of temperature sensor
+const allDeviceIDs = [
+  doorBell,
+  doorBellMotion,
+  overheadLights,
+  floorLights,
+  accentLights,
+  handrailLights,
+  washroomLights,
+  motionSensor,
+  luminSensor,
+  weightSensor,
+  humiditySensor,
+  temperatureSensor,
+];
 // START OF SOCKET ACTIONS
 // ********************************************************************************************************** //
 // Upon starting up a connection we want to...
-socket.onopen = (event) => {
+socket.onopen = async (event) => {
   console.log("WebSocket connection opened:", event);
 
   // ... first authenticate that it is us...
@@ -47,13 +64,14 @@ socket.onopen = (event) => {
     })
   );
   idNumber++;
+  getStates();
 };
 
 // When we receive a message, we want to...
 socket.onmessage = (event) => {
   //... convert the incoming JSON into a usable form and then...
   const data1 = JSON.parse(event.data);
-  console.log("1)Type= ", data1.type); //debug
+  debug("1)Type= ", data1.type); //debug
   try {
     //...  check if it is an "event" type message...
     if (data1.type == "event") {
@@ -61,7 +79,7 @@ socket.onmessage = (event) => {
       if (data1.event.event_type == "state_changed") {
         // ... of a device we are familiar with...
         const deviceID = data1.event.data.entity_id;
-        console.log("2)EntityID: ", deviceID); //debug
+        debug("2)EntityID: ", deviceID); //debug
         //... then we will act accordingly.
         switch (deviceID) {
           case overheadLights:
@@ -83,13 +101,19 @@ socket.onmessage = (event) => {
           case doorBell:
             readDoorbell(data1);
             break;
+          case humiditySensor:
+            readHumiditySensor(data1);
+            break;
+          case temperatureSensor:
+            readTemperatureSensor(data1);
+            break;
           case "sun.sun":
-            console.log("3)Sun Data");
+            debug("3)Sun Data"); //debug
             break;
           default:
             //... unless we actually aren't familiar with it.
-            console.log("3)Unlisted ID: ", deviceID);
-            console.log("3)Payload: ", data1);
+            debug("3)Unlisted ID: ", deviceID); //debug
+            debug("3)Payload: ", data1); //debug
         }
       } else {
         readOtherDataType(data1);
@@ -99,9 +123,9 @@ socket.onmessage = (event) => {
       //If it is a requested state result
       if (data1.id == statID) {
         readResults(data1);
-        console.log("2)Results seeking: ", statDevice);
+        debug("2)Results seeking: ", statDevice); //debug
       } else {
-        console.log("2)Results: ", data1);
+        debug("2)Results: ", data1); //debug
       }
     }
   } catch (error) {
@@ -109,6 +133,18 @@ socket.onmessage = (event) => {
     console.log("Problem JSON: ", JSON.stringify(JSON.parse(event.data)));
   }
 };
+
+//SHOULD NOT BE NECESSARY
+// //On load of window, grab states
+// window.onload = function () {
+//   let int = setInterval(() => {
+//     //Repeat this until status of device has been read
+//     if (isFinallyConnected) {
+//       clearInterval(int);
+//       getStates();
+//     }
+//   }, 1000);
+// };
 
 //Upon Closing Connection
 socket.onclose = (event) => {
@@ -119,6 +155,7 @@ socket.onclose = (event) => {
 function sendMessage(message) {
   console.log("POST: ", message); //debug
   socket.send(message);
+  idNumber++;
 }
 
 //Get request for states
@@ -128,20 +165,19 @@ function getStates() {
     type: "get_states",
   });
   statID = idNumber;
-  idNumber++;
-  console.log(message);
+  debug(message); //debug
   sendMessage(message);
 }
 
 //Grab status
-function getStatus(deviceID) {
+function deviceStatus(deviceID) {
   statDevice = deviceID; //set global variable of device I am seeking
   getStates(); //Request data from Home Assistant
   let int = setInterval(() => {
     //Repeat this until status of device has been read
     if (statFlag) {
       clearInterval(int);
-      console.log("###STAT:", statStatusOfDevice); //DEbug
+      debug("###STAT:", statStatusOfDevice); //DEbug
       statFlag = false;
       return statStatusOfDevice;
     }
@@ -167,13 +203,12 @@ function triggerLightSwitch(buttonID) {
         entity_id: deviceName,
       },
     });
-    idNumber++;
     sendMessage(message);
-    return getStatus(deviceName);
+    return deviceStatus(deviceName);
   } else {
-    console.log(
+    debug(
       "InteractionTemporarilyDisabled^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
-    );
+    ); //debug
     return "NOPE";
   }
 }
@@ -247,15 +282,15 @@ function readLight(data1, deviceID) {
   const newState = data1.event.data.new_state.state;
   const obj = checkWhichLight(deviceID);
   const elementID = obj.elem;
-  console.log("Light: ", deviceID, "; Status: ", newState);
+  debug("Light: ", deviceID, "; Status: ", newState); //debug
   if (newState == "on") {
     elementID.innerHTML = `<img src="../images/lightOn.png">
      <p>${obj.name}</p>
-     <div class="isLightOn">ON</div>`;
+     <div class="lightOn-OffDiv smartLightON" >ON</div>`;
   } else if (newState == "off") {
-    elementID.innerHTML = `<img src="../images/lightOff.png">
+    elementID.innerHTML = `<img src="../images/light.png">
      <p>${obj.name}</p>
-     <div class="isLightOff">OFF</div>`;
+     <div class="lightOn-OffDiv smartLightOFF">OFF</div>`;
   } else {
     console.log("Error: Unknown new light state: ", newState);
   }
@@ -264,16 +299,13 @@ function readLight(data1, deviceID) {
 //How we deal with reading the luminosity levels on the motion sensor
 function readLuminSensor(data1) {
   //REPLACE ALL OF THIS WITH APPROPRIATE THINGS
-  const fLumUp = document.getElementById("fancyTest-LightUp"); //CHANGE
-  const fLumDown = document.getElementById("fancyTest-LightDown"); //CHANGE
   const newState = data1.event.data.new_state.state;
   const oldState = data1.event.data.old_state.state;
+  document.getElementById(
+    "luminosityDiv"
+  ).innerHTML = `Luminosity Level was ${oldState}lux, but is now ${newState}lux.`;
   if (newState > oldState) {
-    fLumUp.style.color = "green"; //CHANGE
-    fLumDown.style.color = "red"; //CHANGE
   } else if (newState <= oldState) {
-    fLumUp.style.color = "red"; //CHANGE
-    fLumDown.style.color = "green"; //CHANGE
   } else {
     console.log("Error: Luminosity level transition unknown");
     console.log("New state= ", newState, "; Old state = ", oldState);
@@ -282,18 +314,15 @@ function readLuminSensor(data1) {
 
 //How we deal with reading the motion sensor detecting motion
 function readMotionSensor(data1) {
-  //REPLACE WITH ALL APPROPRIATE STUFF
-  const mLeft = document.getElementById("fancyTest-MotionLeft"); //CHANGE
-  const mEnter = document.getElementById("fancyTest-MotionEnter"); //CHANGE
-  const mPresence = document.getElementById("fancyTest-MotionPresence"); //CHANGE
   const newState = data1.event.data.new_state.state;
-  console.log("Motion: ", data1); //DEBUG
+  const motionDiv = document.getElementById("motionDiv");
+  // debug("Motion: ", data1); //DEBUG
   if (newState == "on") {
-    mPresence.style.color = "green"; //CHANGE
-    mLeft.style.color = "red"; //CHANGE
+    motionDiv.innerHTML = `Presence Detected at ${data1.event.time_fired}`;
+    debug(`Presence Detected at ${data1.event.time_fired}`);
   } else if (newState == "off") {
-    mPresence.style.color = "red"; //CHANGE
-    mLeft.style.color = "green"; //CHANGE
+    motionDiv.innerHTML = `Presence Left at ${data1.event.time_fired}`;
+    debug(`Presence Detected at ${data1.event.time_fired}`);
   } else {
     console.log("Error: Unknown motion detector state detected: ", newState);
   }
@@ -304,29 +333,79 @@ function readOtherDataType(data1) {
   const eventType = data1.event.event_type;
   switch (eventType) {
     case "call_service":
-      console.log("3)Event Type: call_service");
+      debug("3)Event Type: call_service"); //debug
       break;
     case "config_entry_discovered":
-      console.log("3)Event Type: config_entry_discovered");
+      debug("3)Event Type: config_entry_discovered"); //debug
       break;
     case "recorder_5min_statistics_generated":
-      console.log("3)Event Type: recorder_5min_statistics_generated");
+      debug("3)Event Type: recorder_5min_statistics_generated"); //debug
       break;
     default:
-      console.log("3)Unlisted event type: ", eventType);
+      debug("3)Unlisted event type: ", eventType); //debug
       break;
   }
 
-  console.log("3.5)Payload: ", data1);
+  debug("3.5)Payload: ", data1); //debug
 }
 
 //How we deal with readings from the weight sensor; it detects and stores the value in KG
 function readWeightSensor(data1) {
   //REPLACE ALL ITEMS IN THIS AREA WITH APPROPRIATE FUNCTIONS
-  const weightText = document.getElementById("fancyTestWeight"); //CHANGE
+  // const weightText = document.getElementById("fancyTestWeight"); //CHANGE
   const givenWeightkgs = data1.event.data.new_state.state;
   const givenWeightlbs = givenWeightkgs * 2.2;
-  weightText.innerHTML = `Weight = ${givenWeightkgs}kgs or ${givenWeightlbs}lbs`; //CHANGE
+  debug(`Weight = ${givenWeightkgs}kgs or ${givenWeightlbs}lbs`); //debug
+  document.getElementById(
+    "weightDiv"
+  ).innerHTML = `Weight = ${givenWeightkgs}kgs or ${givenWeightlbs}lbs`;
+  // weightText.innerHTML = `Weight = ${givenWeightkgs}kgs or ${givenWeightlbs}lbs`; //CHANGE
+}
+
+//Read Humidity Sensor
+function readHumiditySensor(data1) {
+  const humidity = data1.event.data.new_state.state;
+  debug("Humidity level is: ", humidity, "%");
+  document.getElementById("humidityDiv").innerHTML = `Humidity is ${humidity}%`;
+}
+
+//Read Temperature Sensor
+function readTemperatureSensor(data1) {
+  const temp = data1.event.data.new_state.state;
+  debug("Temperature is: ", temp, "*c");
+  document.getElementById(
+    "temperatureDiv"
+  ).innerHTML = `Temperature is ${temp}*c`;
+  //CURRENTLY USING THING3 AS A FAN and THING1 AS A HEATER
+  let serviceFan = "";
+  let serviceHeat = "";
+  if (temp > 25) {
+    serviceFan = "turn_on";
+    serviceHeat = "turn_off";
+  } else if (temp < 20) {
+    serviceFan = "turn_off";
+    serviceHeat = "turn_on";
+  }
+  const message = JSON.stringify({
+    id: idNumber,
+    type: "call_service",
+    domain: "switch",
+    service: serviceHeat,
+    service_data: {
+      entity_id: floorLights,
+    },
+  });
+  const message1 = JSON.stringify({
+    id: idNumber,
+    type: "call_service",
+    domain: "switch",
+    service: serviceFan,
+    service_data: {
+      entity_id: accentLights,
+    },
+  });
+  // sendMessage(message);
+  // sendMessage(message1);
 }
 
 //Reads results according to statDevice and updates statStatusOfDevice
@@ -339,7 +418,7 @@ function readResults(data1) {
       statStatusOfDevice = information[i].state;
       statFlag = true;
       setTimeout(enableInteractionFlag(), 1000);
-      console.log("INREAD:", statStatusOfDevice); //DEBUG
+      debug("INREAD:", statStatusOfDevice); //DEBUG
       break;
     }
     i++;
@@ -350,4 +429,30 @@ function enableInteractionFlag() {
   interactFlag = true;
 }
 
-console.log("IoT controls loaded"); //debug
+//Debug Code here; disable by disabling console.log
+function debug(msg0, msg1, msg2, msg3, msg4, msg5) {
+  if (msg0 == undefined) msg0 = "";
+  if (msg1 == undefined) msg1 = "";
+  if (msg2 == undefined) msg2 = "";
+  if (msg3 == undefined) msg3 = "";
+  if (msg4 == undefined) msg4 = "";
+  if (msg5 == undefined) msg5 = "";
+  if (debugFlag) {
+    console.log(msg0, msg1, msg2, msg3, msg4, msg5);
+  }
+}
+
+//NOTE: NON-FUNCTIONAL
+function reloadAll() {
+  debugFlag = false;
+  let stopFlag = false;
+  for (let i = 0; i < allDeviceIDs.length; i++) {
+    if (!stopFlag) {
+      console.log("Reloading1");
+      stopFlag = true;
+      deviceStatus(allDeviceIDs[i]);
+    }
+  }
+}
+
+debug("IoT controls loaded"); //debug
